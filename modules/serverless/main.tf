2dc04cd285ba8f4174d37aaac8046358344390ef
aws2gcp <jeheyer@icloud.com>
2022-12-04 11:46:49 -0800

    updates

--- modules/serverless/main.tf
+++ modules/serverless/main.tf
@@ -1,34 +1,39 @@
 locals {
   create            = coalesce(var.create, true)
+  use_random_name   = var.name == null ? true : false
   name              = local.use_random_name ? try(lower(one(random_string.name).result), null) : lower(var.name)
   description       = try(lower(var.description), null)
-  use_random_name   = var.name == null ? true : false
   is_cloud_function = false
+  version           = local.is_cloud_function ? var.params.cloud_function_version : null
   is_cloud_run      = var.params.image != null ? true : false
   is_app_engine     = false
-  version           = 1
   location          = lower(var.params.region)
+  timeout           = var.params.timeout
 }
 
+# Generate a random name, if required
 resource "random_string" "name" {
   count   = local.use_random_name ? 1 : 0
   length  = 31
   special = false
 }
 
+# Cloud Function v1
 resource "google_cloudfunctions_function" "default" {
-  count               = local.create && local.is_cloud_function && local.version == 1 ? 1 : 0
-  name                = local.name
-  description         = local.description
-  runtime             = var.params.runtime
-  available_memory_mb = var.params.available_memory
-  #source_archive_bucket = google_storage_bucket.bucket.name
-  #source_archive_object = google_storage_bucket_object.archive.name
-  trigger_http = var.params.trigger_http
-  entry_point  = var.params.entry_point
-  project      = var.project_id
+  count                 = local.create && local.is_cloud_function && local.version == 1 ? 1 : 0
+  name                  = local.name
+  description           = local.description
+  region                = var.params.region
+  runtime               = var.params.runtime
+  available_memory_mb   = var.params.available_memory
+  trigger_http          = var.params.trigger_http
+  entry_point           = var.params.entry_point
+  environment_variables = var.params.environment_variables
+  timeout               = local.timeout
+  project               = var.project_id
 }
 
+# Cloud Function v2
 resource "google_cloudfunctions2_function" "default" {
   count       = local.create && local.is_cloud_function && local.version == 2 ? 1 : 0
   name        = local.name
@@ -39,14 +44,20 @@ resource "google_cloudfunctions2_function" "default" {
     entry_point = var.params.entry_point
   }
   service_config {
-    available_memory   = "${upper(var.params.available_memory)}${var.params.available_memory > 1000 ? "G" : "M"}"
-    min_instance_count = var.params.min_instances
-    max_instance_count = var.params.max_instances
-    timeout_seconds    = var.params.timeout
+    available_memory               = "${upper(var.params.available_memory)}${var.params.available_memory > 999 ? "G" : "M"}"
+    min_instance_count             = var.params.min_instances
+    max_instance_count             = var.params.max_instances
+    timeout_seconds                = var.params.timeout
+    ingress_settings               = "ALLOW_ALL"
+    all_traffic_on_latest_revision = true
+    #versions {
+    #  version = "latest"
+    #}
   }
   project = var.project_id
 }
 
+# Cloud Run Service
 resource "google_cloud_run_service" "default" {
   count    = local.create && local.is_cloud_run ? 1 : 0
   name     = local.name
@@ -56,7 +67,8 @@ resource "google_cloud_run_service" "default" {
       containers {
         image = var.params.image
         ports {
-          protocol       = "TCP"
+          name = "http1"
+          #protocol       = "TCP"
           container_port = var.params.container_ports[0]
         }
       }
@@ -69,6 +81,7 @@ resource "google_cloud_run_service" "default" {
   project = var.project_id
 }
 
+# App Engine
 resource "google_app_engine_application" "default" {
   count       = local.create && local.is_app_engine ? 1 : 0
   location_id = local.location
--- modules/serverless/variables.tf
+++ modules/serverless/variables.tf
@@ -20,16 +20,23 @@ variable "create" {
 variable "params" {
   description = "Parameters for this Serverless Application"
   type = object({
-    region           = optional(string, "us-central1")
-    runtime          = optional(string, "nodejs16")
-    available_memory = optional(number, 128)
-    trigger_http     = optional(bool, true)
-    entry_point      = optional(string, "function")
-    min_instances    = optional(number, 0)
-    max_instances    = optional(number, 2)
-    timeout          = optional(number, 60)
-    image            = optional(string)
-    container_ports  = optional(list(number), [8080])
+    region                 = optional(string, "us-central1")
+    runtime                = optional(string, "nodejs16")
+    available_memory       = optional(number, 128)
+    trigger_http           = optional(bool, true)
+    entry_point            = optional(string, "function")
+    min_instances          = optional(number, 0)
+    max_instances          = optional(number, 2)
+    timeout                = optional(number, 60)
+    environment_variables  = optional(map, {})
+    image                  = optional(string)
+    container_ports        = optional(list(number), [8080])
+    vpc_connector          = optional(string)
+    cloud_function_version = optional(number, 1)
   })
+  validation {
+    condition     = var.params.available_memory >= 16 && var.params.available_memory <= 4096
+    error_message = "Memory size must be between 16 MB and 4 GB"
+  }
   default = {}
 }
