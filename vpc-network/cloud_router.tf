1122df3be86f4a260b8fd60a03d0197a8c898a10
John Heyer <jheyer@otldap.net>
2023-05-08 11:38:01 -0700

    add region param

--- vpc-network/cloud_router.tf
+++ vpc-network/cloud_router.tf
@@ -3,6 +3,7 @@
 module "cloud_routers" {
   source        = "../hybrid-networking/"
   project_id    = var.project_id
+  region        = var.region
   network_name  = var.network_name
   cloud_routers = var.cloud_routers
   depends_on    = [google_compute_network.default]
