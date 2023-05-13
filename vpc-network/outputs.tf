3349b8949b66d78f33bf7afe2db03a9576e12a19
aws2gcp <jeheyer@icloud.com>
2023-05-12 20:10:10 -0700

    more outputs

--- vpc-network/outputs.tf
+++ vpc-network/outputs.tf
@@ -1,3 +1,12 @@
+output "network_name" {
+  value = google_compute_network.default.name
+}
+output "network_id" {
+  value = google_compute_network.default.id
+}
+output "network_self_link" {
+  value = google_compute_network.default.self_link
+}
 output "subnets" {
   value = { for k, v in local.subnets : k => {
     name     = v.name
