1b571b5e3883632e1344b270ba229496340d0438
aws2gcp <jeheyer@icloud.com>
2023-05-16 11:05:22 -0700

    add output

--- mig/outputs.tf
+++ mig/outputs.tf
@@ -2,3 +2,4 @@ output "id" { value = google_compute_region_instance_group_manager.default.id }
 output "name" { value = google_compute_region_instance_group_manager.default.name }
 output "self_link" { value = google_compute_region_instance_group_manager.default.self_link }
 output "instance_group" { value = google_compute_region_instance_group_manager.default.instance_group }
+output "list_managed_instances_results" { value = google_compute_region_instance_group_manager.default.list_managed_instances_results }
