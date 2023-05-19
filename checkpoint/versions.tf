a0299e03e57c7eb84e6bb16033c5d9707dea5d01
John Heyer <jheyer@otldap.net>
2023-05-18 21:18:27 -0700

    update versions

--- checkpoint/versions.tf
+++ checkpoint/versions.tf
@@ -2,7 +2,8 @@ terraform {
   required_version = ">= 1.3.4"
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
+      version = ">= 4.49, < 5.0"
     }
   }
 }
