e8dfe30603bf46d6d966e7e3b7d6ee00acb42cef
AWS & GCP Networking <114036001+aws2gcp@users.noreply.github.com>
2023-05-20 17:33:35 -0700

    add more outputs

--- psc-endpoint/outputs.tf
+++ psc-endpoint/outputs.tf
@@ -1,2 +1,6 @@
 output "name" { value = local.name }
 output "address" { value = google_compute_address.default.address }
+output "address_name" { value = google_compute_address.default.name }
+output "psc_connection_id" {
+  value = local.create ? one(google_compute_forwarding_rule.default).psc_connection_id : null
+}
