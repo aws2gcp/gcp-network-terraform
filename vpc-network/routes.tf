02e0832e73a30824ccd81365a747f5ed82c76fa0
aws2gcp <jeheyer@icloud.com>
2023-11-14 20:25:09 -0800

    improve functionality as module

--- vpc-network/routes.tf
+++ vpc-network/routes.tf
@@ -4,17 +4,20 @@ locals {
     project_id    = coalesce(v.project_id, var.project_id)
     name          = replace(coalesce(v.name, "route-${i}"), "_", "-")
     next_hop_type = can(regex("^[1-2]", v.next_hop)) ? "ip" : "instance"
-    dest_ranges   = coalesce(v.dest_ranges, [v.dest_range], [])
+    #dest_ranges   = coalesce(v.dest_ranges, [v.dest_range], [])
   })]
-  routes = flatten([
-    for i, v in local.routes_0 : [
+  routes = flatten(concat(
+    [for i, v in local.routes_0 : [
       for r, dest_range in coalesce(v.dest_ranges, []) : merge(v, {
         name       = "${v.name}-${r}"
         key        = "${v.project_id}::${v.name}::${r}"
         dest_range = dest_range
       })
-    ]
-  ])
+    ]],
+    [for i, v in local.routes_0 : merge(v, {
+      key = "${v.project_id}::${v.name}"
+    }) if v.dest_range != null]
+  ))
 }
 
 # Static Routes
