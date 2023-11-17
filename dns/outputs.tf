3c38fe31cb1b9d5cc8e25ef470a2bcdb1aec599b
aws2gcp <jeheyer@icloud.com>
2023-11-16 17:54:53 -0800

    fix key in records

--- dns/outputs.tf
+++ dns/outputs.tf
@@ -1,8 +1,8 @@
 output "dns_zones" {
   value = {
-    for k, v in local.dns_zones : v.key => {
-      dns_name     = try(google_dns_managed_zone.default[k].dns_name, null)
-      name_servers = try(google_dns_managed_zone.default[k].name_servers, null)
+    for i, v in local.dns_zones : v.key => {
+      dns_name     = try(google_dns_managed_zone.default[v.key].dns_name, null)
+      name_servers = try(google_dns_managed_zone.default[v.key].name_servers, null)
       visibiltiy   = v.visibility
     } if v.create
   }
